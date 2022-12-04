db1d00c4d1286269e55c30fb3614c9983beb7424
EngOmarDraidi <omar.m3z2001@gmail.com>
2022-12-04 08:26:20 +0200

    Create Relation (User - Review)

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from rest_framework.views import APIView
 # from rest_framework.generics import GenericAPIView
 # from rest_framework import mixins
@@ -23,10 +24,18 @@ class ReviewDetails(generics.RetrieveUpdateDestroyAPIView):
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        return Review.objects.all()
+
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         movie = WatchList.objects.get(pk=pk)
 
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist = movie, review_user = review_user)
+
+        if review_queryset.exists():
+            raise ValidationError('You have already reviewed this model')
         serializer.save(watchlist = movie)
 
 # class ReviewList(mixins.ListModelMixin,
