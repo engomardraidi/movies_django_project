b10674a0bda001ef9ec80618ac6c976f595313d5
EngOmarDraidi <omar.m3z2001@gmail.com>
2022-12-09 08:45:05 +0200

    Token Auhtentication - Part 6 - Logout

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -5,6 +5,12 @@ from rest_framework import status
 from .serializers import RegistrationSerializer
 
 @api_view(['POST'])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status = status.HTTP_200_OK)
+
+@api_view(['POST'])
 def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data = request.data)
