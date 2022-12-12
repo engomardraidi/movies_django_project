28b2e23f014a8150dc0f8cb5a032fc00fa0c8e95
EngOmarDraidi <omar.m3z2001@gmail.com>
2022-12-12 06:21:02 +0200

    JWT Authentication - AT / RT

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns = [
     path('login/', obtain_auth_token, name = 'login'),
     path('register/', views.registration_view, name = 'register'),
     path('logout/', views.logout_view, name = 'logout'),
+    path('token/', TokenObtainPairView.as_view(), name = 'token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
 ]
